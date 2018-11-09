2f222df3ca0568009b8033e2e6c0d60ce166f53e
Someonelya <43191409+Someonelya@users.noreply.github.com>
2018-11-09 09:03:54 +0800

    EX5

---
+++ ex5_2.py
@@ -0,0 +1,6 @@
+def isOdd(n) :
+    n = int(n)
+    if n % 2 == 0 :
+        return True
+    else :
+        return False
---
+++ ex5_4.py
@@ -0,0 +1,2 @@
+N = int(input("请输入数字："))
+c = [int(input()) for i in range(N)]
